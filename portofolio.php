9484a0d3dbb03af900a1fcc00dd5776a01e6a79e
asyafii27 <86064317+asyafii27@users.noreply.github.com>
2022-10-07 10:57:00 +0700

    bagian portofolio

---
+++ portofolio.php
@@ -0,0 +1,36 @@
+<!-- ======= Services Section ======= -->
+<section id="services" class="services">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2 class="mt-4">Portofolio</h2>
+            <p>Bermimpilah selagi kamu bisa</p>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon-box iconbox-blue">
+                        <img src="assets/img/portfolio/porto 1.jpg" class="img-fluid" alt="">
+                    <h4><a href="">Purchasing System</a></h4>
+                    <p>Ini adalah sistem informasi Purchasing yang dibangung menggunakan Laravel</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon-box iconbox-blue">
+                        <img src="assets/img/portfolio/porto 2.jpg" class="img-fluid" alt="">
+                    <h4><a href="">Alat Monitoring Epilepsi</a></h4>
+                    <p>Ini adalah prototipe alat monitoring Epilepsi yang berhasil mendapatkan pendanaan dari Belmawa</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon-box iconbox-blue">
+                        <img src="assets/img/portfolio/porto 3.jpg" class="img-fluid" alt="">
+                    <h4><a href="">Penerapan CNN pada klasifikasi Hijaiyah</a></h4>
+                    <p>Ini adalah artikel Ilmiah yang berhasil mendapatkan Juara II dalam ajang Dimas-TI tingkat LPTK</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- End Services Section -->
