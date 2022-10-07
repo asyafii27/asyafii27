8e6b7e3a01e22b63562bcfd5077ae19b281f0016
asyafii27 <86064317+asyafii27@users.noreply.github.com>
2022-10-07 10:57:36 +0700

    bagian study

---
+++ study.php
@@ -0,0 +1,82 @@
+  <section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+          <div class="section-title">
+              <h2 class="mt-4">Education</h2>
+              <p>Riwayat Pendidikan</p>
+          </div>
+
+          <div class="row">
+              <div class="col-lg-6">
+                  <h3 class="resume-title">SD Negeri Sugihan</h3>
+                  <div class="resume-item">
+                      <h5>2007 - 2013</h5>
+                      <p><em></em></p>Prestasi
+                      <ul>
+                          <li>Mendapatkan Ranking II kelas 5 SD</li>
+                          <li>Peringkat 5 besar Ujian Nasional di kelas </li>
+
+                      </ul>
+                      <p>Organisasi</p>
+                      <ul>
+                          <li>Peserta Pesta Siaga Tingkat Kabupaten</li>
+                          <li>Peserta MAPSI cabang Kaligrafi tingkat Kecamatan</li>
+
+                      </ul>
+                  </div>
+
+                  <h3 class="resume-title">SMP Negeri 1 Winong </h3>
+                  <div class="resume-item">
+                      <h5>2013 - 2016</h5>
+                      <p>Prestasi</p>
+                      <ul>
+                          <li>Mendapatkan Ranking II kelas 8</li>
+                          <li>Mengikuti lomba karya ilmiah remaja (KIR) cabang Teknologi Rekayasa tingkat Kabupaten</li>
+
+                      </ul>
+                      <p>Organisasi</p>
+                      <ul>
+                          <li>Anggota Tim Karya Ilmiah Remaja</li>
+                          <li>Anggota dalam Regu Inti Pramuka (Guinka)</li>
+
+                      </ul>
+                  </div>
+              </div>
+              <div class="col-lg-6">
+                  <h3 class="resume-title">SMA Negeri 2 Pati</h3>
+                  <div class="resume-item">
+                      <h5>2017 - 2020</h5>
+                      <p>Prestasi</p>
+                      <ul>
+                          <li>Mendapatkan Ranking II kelas 12</li>
+                          <li>Peserta Lomba OPSI (Olimpiade Penelitian Siswa Indonesia)</li>
+
+                      </ul>
+                      <p>Organisasi</p>
+                      <ul>
+                          <li>Anggota Tim Karya Ilmiah Sekolaj</li>
+                          <li>Ketua Departemen Seni Budaya dan Olahraga (DSBO) pada ROHIS (Kerohanian Islam)</li>
+
+                      </ul>
+                  </div>
+                  <div class="resume-item">
+                      <h4>Universitas Negeri Semarang</h4>
+                      <h5>2020 - Sekarang</h5>
+                      <p>Prestasi</p>
+                      <ul>
+                          <li>Juara II Lomba KTI Dimas TI tingkat LPTK</li>
+                          <li>Lolos Pendanaan dari Belmawa pada Program Kreativitas Mahasiswa Karsa Cipta (PKM-KC) 2022.</li>
+
+                      </ul>
+                      <p>Organisasi</p>
+                      <ul>
+                          <li>Ketua Divisi Research I-Secret 2022</li>
+                          <li>Staff Ahli Himpunan Pengusaha Muda (HIPMI) tingkat universitas</li>
+                          <li>Ketua Tim Program Kreativitas Mahasiswa Karsa Cipta (PKM-KC) 2022</li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+  </section><!-- End Resume Section -->
